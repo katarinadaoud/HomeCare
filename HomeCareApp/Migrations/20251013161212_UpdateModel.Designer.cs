01999d95173ec741bf984a96f47ce8d703bf5056
susannegulbrandsen <143507850+susannegulbrandsen@users.noreply.github.com>
2025-10-13 18:13:33 +0200

    oppdatert databasen

--- HomeCareApp/Migrations/20251013161212_UpdateModel.Designer.cs
+++ HomeCareApp/Migrations/20251013161212_UpdateModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HomeCareApp.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeCareApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251013161212_UpdateModel")]
+    partial class UpdateModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
