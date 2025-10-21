c421eec341afc976a613328e01ac65c98b916f61
naban2910 <naban2910@oslomet.no>
2025-10-21 17:28:14 +0200

    engelske kommentarer

--- HomeCareApp/DAL/IPatientRepository.cs
+++ HomeCareApp/DAL/IPatientRepository.cs
@@ -8,12 +8,6 @@ public interface IPatientRepository
     Task<IEnumerable<Patient>> GetAll();
     Task<Patient?> GetItemById(int id);
     Task Create(Patient patient);
-<<<<<<< HEAD
-
-    Task Update(Patient patient); // updates patient
-    Task<bool> Delete(int id); // deletes patient by id
-=======
     Task Update(Patient patient);
     Task<bool> Delete(int id);
->>>>>>> e42e3a3d3a827b2ffc5122b206670b784f2e03d7
 }
